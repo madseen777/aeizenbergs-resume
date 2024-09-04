7a1e13fa3a52f2e3f0f52d8bf8f2ccec747f02c3
Andris Eizenbergs <andris.eizenbergs@4finance.com>
2024-09-04 20:36:30 +0300

    Adding Page view counter

--- website/index.js
+++ website/index.js
@@ -1,8 +1,8 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://jnfrl373qruq2u6voj2qjdwvpm0ohzvz.lambda-url.eu-west-1.on.aws/");
+    let response = await fetch("https://tchupd6oa6brrjgeqgjw2i2nau0uoxag.lambda-url.eu-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    counter.innerHTML = `${data}`;
 }
 
 updateCounter();
